ef63f37972b421edb2459e0205142347a26ef4eb
Janjan <jannatividad1313@gmail.com>
2025-01-05 01:26:47 +0800

    feat(route): add root route to render index.ejs with dynamic daytype and advice

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import express from "express"; // Import from express
 const app = express(); // Initialize an Express app
 const port = 3000; // define port number
 
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    daytype: "A weekday",
+    advice: "it's time to work hard",
+  });
+});
+
 // Start the server and listen on the specified port
 const server = app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
